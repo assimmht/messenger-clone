cfbf0f2f1aebdacb703acc48c8739b5b547f3423
assimmht <assimmht235@gmail.com>
2020-11-02 19:04:11 +0100

    Updates

---
+++ precache-manifest.87b419d421e7f62253b5df0c262ad069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf1cc655b7118edc2eb04cd2580f999",
+    "url": "/messenger-clone/index.html"
+  },
+  {
+    "revision": "38aee6f48865e7bae096",
+    "url": "/messenger-clone/static/css/main.6434cb6f.chunk.css"
+  },
+  {
+    "revision": "4451ad210b0a20d0b40a",
+    "url": "/messenger-clone/static/js/2.101cbdce.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/messenger-clone/static/js/2.101cbdce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38aee6f48865e7bae096",
+    "url": "/messenger-clone/static/js/main.370d4ec9.chunk.js"
+  },
+  {
+    "revision": "ec85557f6cfdb048b3bb",
+    "url": "/messenger-clone/static/js/runtime-main.76eefcd8.js"
+  }
+]);
